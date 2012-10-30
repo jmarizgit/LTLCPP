f4301f252d1cb5f37cd03d0a3b2d5573cdbb00b5
Mariz Melo <m@emoriz.com>
2012-10-29 20:41:35 -0700

    using namespace for std (standard library)

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,12 @@
 
 //start of program
 int main(){
-	std::cout << "Hello world!" << std::endl;
+	//tell compile what namespace to search
+	using namespace std;
+
+	//prints message to console
+	//std::cout << "Hello world!" << std::endl;
+	/*new example without std::*/
+	cout << "Hello world!" << endl;
+
 }//main()
