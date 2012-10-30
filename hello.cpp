801acafda5b4f26cab834c1b992057a49a503114
Mariz Melo <m@emoriz.com>
2012-10-29 20:45:28 -0700

    more restrictive use for namespacing

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,12 @@
 //start of program
 int main(){
 	//tell compile what namespace to search
-	using namespace std;
+	//using namespace std;
+
+	//more restrictive example of namespace
+	//only hides std from "cout" and "endl", not other methods
+	using std::cout;
+	using std::endl;
 
 	//prints message to console
 	//std::cout << "Hello world!" << std::endl;
